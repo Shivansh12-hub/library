b58a6166f809a260c6c6e1626f1ad624209bfc06
Shivansh Kumar <shivanshcoc11@gmail.com>
2026-09-12 17:33:00 +0530

    backend server is running

--- Backend/app.js
+++ Backend/app.js
@@ -21,8 +21,12 @@ app.use('/api/v1/owner', ownerRoutes);
 app.use('/api/v1/admin', adminRoutes);
 
 // Catch-all route not found
-app.use('*', (req, res, next) => {
-  res.status(404).json({ success: false, message: `Route ${req.originalUrl} not found` });
+// Catch-all route not found
+app.use((req, res, next) => {
+  res.status(404).json({ 
+    success: false, 
+    message: `Route ${req.originalUrl} not found` 
+  });
 });
 
 // Central Error Middleware
